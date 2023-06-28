e24394f3cc5e8b54f88591f97104359bbad4ab77
udhos <udhos>
2023-06-28 01:54:54 -0300

    Remove unused config.

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,32 +5,28 @@ import "github.com/udhos/opentelemetry-trace-sqs/internal/env"
 
 // AppConfig holds application configuration.
 type AppConfig struct {
-	HTTPAddr               string
-	HTTPRoute              string
-	JaegerURL              string
-	QueueURLInput          string
-	QueueURLOutput         string
-	QueueRoleARNInput      string
-	QueueRoleARNOutput     string
-	QueueTraceIDAttrInput  string
-	QueueTraceIDAttrOutput string
-	BackendURL             string
-	EndpointURL            string
+	HTTPAddr           string
+	HTTPRoute          string
+	JaegerURL          string
+	QueueURLInput      string
+	QueueURLOutput     string
+	QueueRoleARNInput  string
+	QueueRoleARNOutput string
+	BackendURL         string
+	EndpointURL        string
 }
 
 // New loads configuration from env vars.
 func New() AppConfig {
 	return AppConfig{
-		HTTPAddr:               env.String("HTTP_ADDR", ":8001"),
-		HTTPRoute:              env.String("HTTP_ROUTE", "/send"),
-		JaegerURL:              env.String("JAEGER_URL", "http://jaeger-collector:14268/api/traces"),
-		QueueURLInput:          env.String("QUEUE_URL_INPUT", ""),
-		QueueURLOutput:         env.String("QUEUE_URL_OUTPUT", ""),
-		QueueRoleARNInput:      env.String("QUEUE_ROLE_ARN_INPUT", ""),
-		QueueRoleARNOutput:     env.String("QUEUE_ROLE_ARN_OUTPUT", ""),
-		QueueTraceIDAttrInput:  env.String("QUEUE_TRACE_ID_ATTR_INPUT", "traceId"),
-		QueueTraceIDAttrOutput: env.String("QUEUE_TRACE_ID_ATTR_OUTPUT", "traceId"),
-		BackendURL:             env.String("BACKEND_URL", "http://localhost:8002/send"),
-		EndpointURL:            env.String("ENDPOINT_URL", ""),
+		HTTPAddr:           env.String("HTTP_ADDR", ":8001"),
+		HTTPRoute:          env.String("HTTP_ROUTE", "/send"),
+		JaegerURL:          env.String("JAEGER_URL", "http://jaeger-collector:14268/api/traces"),
+		QueueURLInput:      env.String("QUEUE_URL_INPUT", ""),
+		QueueURLOutput:     env.String("QUEUE_URL_OUTPUT", ""),
+		QueueRoleARNInput:  env.String("QUEUE_ROLE_ARN_INPUT", ""),
+		QueueRoleARNOutput: env.String("QUEUE_ROLE_ARN_OUTPUT", ""),
+		BackendURL:         env.String("BACKEND_URL", "http://localhost:8002/send"),
+		EndpointURL:        env.String("ENDPOINT_URL", ""),
 	}
 }
