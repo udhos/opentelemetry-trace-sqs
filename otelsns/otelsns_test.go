194f0ab73afef21d5319128835197bb502bbea97
udhos <udhos>
2023-10-20 16:44:09 -0300

    Add tests.

--- otelsns/otelsns_test.go
+++ otelsns/otelsns_test.go
@@ -1,10 +1,112 @@
 package otelsns
 
 import (
+	"context"
+	"log"
 	"testing"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/sns"
+	sqs_types "github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/udhos/opentelemetry-trace-sqs/internal/env"
+	"github.com/udhos/opentelemetry-trace-sqs/internal/tracing"
+	"github.com/udhos/opentelemetry-trace-sqs/otelsqs"
 )
+
+func TestSnSInjectExtract(t *testing.T) {
+	//
+	// initialize tracing
+	//
+	const me = "TestSnSInjectExtract"
+
+	jaegerURL := env.String("JAEGER_URL", "http://jaeger-collector:14268/api/traces")
+
+	var tracer trace.Tracer
+
+	{
+		tp, errTracer := tracing.TracerProvider(me, jaegerURL)
+		if errTracer != nil {
+			log.Fatalf("tracer provider: %v", errTracer)
+		}
+
+		// Register our TracerProvider as the global so any imported
+		// instrumentation in the future will default to using it.
+		otel.SetTracerProvider(tp)
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		// Cleanly shutdown and flush telemetry when the application exits.
+		defer func(ctx context.Context) {
+			// Do not make the application hang when it is shutdown.
+			ctx, cancel = context.WithTimeout(ctx, time.Second*5)
+			defer cancel()
+			if err := tp.Shutdown(ctx); err != nil {
+				log.Fatalf("trace shutdown: %v", err)
+			}
+		}(ctx)
+
+		tracing.TracePropagation()
+
+		tracer = tp.Tracer(me)
+	}
+
+	ctx, span := tracer.Start(context.TODO(), me)
+	defer span.End()
+
+	traceIDSent := span.SpanContext().TraceID().String()
+
+	t.Logf("traceIDSent:%s", traceIDSent)
+
+	//
+	// Send
+	//
+
+	info := "hello"
+	input := sns.PublishInput{
+		Message: aws.String(info),
+	}
+	carrier := NewCarrier()
+	carrier.Inject(ctx, &input)
+
+	//
+	// Receive
+	//
+
+	msg := sqs_types.Message{}
+
+	copyAttributes(&input, &msg)
+
+	carrierSQS := otelsqs.NewCarrier()
+	ctxNew := carrierSQS.Extract(&msg)
+
+	_, span2 := tracer.Start(ctxNew, me)
+	defer span2.End()
+
+	traceIDRecv := span2.SpanContext().TraceID().String()
+
+	t.Logf("traceIDRecv:%s", traceIDRecv)
+
+	if traceIDSent != traceIDRecv {
+		t.Errorf("traceIDSent:%s mismatches traceIDRecv:%s", traceIDSent, traceIDRecv)
+	}
+}
+
+func copyAttributes(from *sns.PublishInput, to *sqs_types.Message) {
+	attr := map[string]sqs_types.MessageAttributeValue{}
+	for k, v := range from.MessageAttributes {
+		attr[k] = sqs_types.MessageAttributeValue{
+			DataType:    v.DataType,
+			BinaryValue: v.BinaryValue,
+			StringValue: v.StringValue,
+		}
+	}
+	to.MessageAttributes = attr
+}
 
 func TestSqsCarrierAttributes(t *testing.T) {
 	input := sns.PublishInput{}
--- otelsqs/otelsqs_test.go
+++ otelsqs/otelsqs_test.go
@@ -1,10 +1,93 @@
 package otelsqs
 
 import (
+	"context"
+	"log"
 	"testing"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/udhos/opentelemetry-trace-sqs/internal/env"
+	"github.com/udhos/opentelemetry-trace-sqs/internal/tracing"
 )
+
+func TestSqsInjectExtract(t *testing.T) {
+	//
+	// initialize tracing
+	//
+	const me = "TestSqsInjectExtract"
+
+	jaegerURL := env.String("JAEGER_URL", "http://jaeger-collector:14268/api/traces")
+
+	var tracer trace.Tracer
+
+	{
+		tp, errTracer := tracing.TracerProvider(me, jaegerURL)
+		if errTracer != nil {
+			log.Fatalf("tracer provider: %v", errTracer)
+		}
+
+		// Register our TracerProvider as the global so any imported
+		// instrumentation in the future will default to using it.
+		otel.SetTracerProvider(tp)
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		// Cleanly shutdown and flush telemetry when the application exits.
+		defer func(ctx context.Context) {
+			// Do not make the application hang when it is shutdown.
+			ctx, cancel = context.WithTimeout(ctx, time.Second*5)
+			defer cancel()
+			if err := tp.Shutdown(ctx); err != nil {
+				log.Fatalf("trace shutdown: %v", err)
+			}
+		}(ctx)
+
+		tracing.TracePropagation()
+
+		tracer = tp.Tracer(me)
+	}
+
+	ctx, span := tracer.Start(context.TODO(), me)
+	defer span.End()
+
+	traceIDSent := span.SpanContext().TraceID().String()
+
+	t.Logf("traceIDSent:%s", traceIDSent)
+
+	//
+	// Send
+	//
+
+	info := "hello"
+	msg := types.Message{
+		Body: aws.String(info),
+	}
+	carrier := NewCarrier()
+	carrier.Inject(ctx, &msg)
+
+	//
+	// Receive
+	//
+
+	ctxNew := carrier.Extract(&msg)
+
+	_, span2 := tracer.Start(ctxNew, me)
+	defer span2.End()
+
+	traceIDRecv := span2.SpanContext().TraceID().String()
+
+	t.Logf("traceIDRecv:%s", traceIDRecv)
+
+	if traceIDSent != traceIDRecv {
+		t.Errorf("traceIDSent:%s mismatches traceIDRecv:%s", traceIDSent, traceIDRecv)
+	}
+}
 
 func TestSqsCarrierAttributes(t *testing.T) {
 	sqsMessage := types.Message{}
