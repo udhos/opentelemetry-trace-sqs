2362a8769137b6df56b1f62c0398395b24a99bb3
udhos <udhos>
2026-03-17 21:15:26 -0300

    Upgrade toolchain.

--- build.sh
+++ build.sh
@@ -3,16 +3,28 @@
 go install golang.org/x/vuln/cmd/govulncheck@latest
 go install golang.org/x/tools/cmd/deadcode@latest
 go install github.com/mgechev/revive@latest
+go install golang.org/x/tools/go/analysis/passes/modernize/cmd/modernize@latest
+go install github.com/gordonklaus/ineffassign@latest
+go install github.com/client9/misspell/cmd/misspell@latest
+go install github.com/fzipp/gocyclo/cmd/gocyclo@latest
 
 gofmt -s -w .
 
 revive ./...
 
+modernize -fix ./...
+
 gocyclo -over 15 .
+
+ineffassign ./...
+
+misspell .
 
 go mod tidy
 
 govulncheck ./...
+
+deadcode ./cmd/*
 
 go env -w CGO_ENABLED=1
 
